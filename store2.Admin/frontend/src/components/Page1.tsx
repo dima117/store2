3555d55fe2e4a3416e2d7d2666f83947523948b4
Dmitry Andriyanov <dima117a@yandex-team.ru>
2019-01-15 23:23:48 +0300

    react-material-ui

--- store2.Admin/frontend/src/components/Page1.tsx
+++ store2.Admin/frontend/src/components/Page1.tsx
@@ -1,5 +1,10 @@
 import * as React from 'react';
 import { Helmet } from 'react-helmet';
+import { cn } from '@bem-react/classname';
+
+import Button from '@material-ui/core/Button';
+
+const cnPage1 = cn('Page1');
 
 export interface StateProps {
     login: string;
@@ -14,15 +19,15 @@ export interface DispatchProps {
 export class Page1 extends React.Component<StateProps & DispatchProps> {
 
     onClickBtnInc = (e: React.MouseEvent<HTMLButtonElement>) => {
-        if (e.target instanceof HTMLButtonElement) {
-            const value = Number(e.target.dataset.value);
+        if (e.currentTarget instanceof HTMLButtonElement) {
+            const value = Number(e.currentTarget.dataset.value);
             this.props.onIncrement(value);
         }
     }
 
     onClickBtnDec = (e: React.MouseEvent<HTMLButtonElement>) => {
-        if (e.target instanceof HTMLButtonElement) {
-            const value = Number(e.target.dataset.value);
+        if (e.currentTarget instanceof HTMLButtonElement) {
+            const value = Number(e.currentTarget.dataset.value);
             this.props.onDecrement(value);
         }
     }
@@ -39,10 +44,10 @@ export class Page1 extends React.Component<StateProps & DispatchProps> {
             <p>login: {login}</p>
             <p>counter: {count}</p>
             <p>
-                <button onClick={this.onClickBtnInc} data-value="1">+1</button>
-                <button onClick={this.onClickBtnInc} data-value="10">+10</button>
-                <button onClick={this.onClickBtnDec} data-value="10">-10</button>
-                <button onClick={this.onClickBtnDec} data-value="1">-1</button>
+                <Button className={cnPage1('Button')} variant="contained" color="primary" onClick={this.onClickBtnInc} data-value="1">+1</Button>
+                <Button className={cnPage1('Button')} variant="contained" color="secondary" onClick={this.onClickBtnInc} data-value="10">+1</Button>
+                <Button className={cnPage1('Button')} variant="contained" color="default" onClick={this.onClickBtnDec} data-value="10">-10</Button>
+                <Button className={cnPage1('Button')} variant="contained" color="inherit" onClick={this.onClickBtnDec} data-value="1">-1</Button>
             </p>
         </>;
     }
